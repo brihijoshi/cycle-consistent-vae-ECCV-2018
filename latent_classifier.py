70c03ad7d40cc6781708858446482f31cc650d40
brihijoshi <brihi16142@iiitd.ac.in>
2020-05-09 20:18:35 +0530

    latent space classifiers written, analysis started

--- latent_classifier.py
+++ latent_classifier.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from collections import OrderedDict
 from torch.nn import functional as F
 
 
@@ -8,15 +9,15 @@ class Classifier(nn.Module):
         super(Classifier, self).__init__()
 
         self.fc_model = nn.Sequential(OrderedDict([
-            ('fc_1', nn.Linear(in_features=z_dim, out_features=256, bias=True)),
-            ('fc_1_bn', nn.BatchNorm1d(num_features=256)),
+            ('fc_1', nn.Linear(in_features=z_dim, out_features=2048, bias=True)),
+#             ('fc_1_bn', nn.BatchNorm1d(num_features=2048)),
             ('leakyrelu_1', nn.LeakyReLU(negative_slope=0.2, inplace=True)),
 
-            ('fc_2', nn.Linear(in_features=256, out_features=256, bias=True)),
-            ('fc_2_bn', nn.BatchNorm1d(num_features=256)),
+            ('fc_2', nn.Linear(in_features=2048, out_features=1024, bias=True)),
+#             ('fc_2_bn', nn.BatchNorm1d(num_features=1024)),
             ('leakyrelu_2', nn.LeakyReLU(negative_slope=0.2, inplace=True)),
 
-            ('fc_3', nn.Linear(in_features=256, out_features=num_classes, bias=True))
+            ('fc_3', nn.Linear(in_features=1024, out_features=num_classes, bias=True))
         ]))
 
     def forward(self, z):
